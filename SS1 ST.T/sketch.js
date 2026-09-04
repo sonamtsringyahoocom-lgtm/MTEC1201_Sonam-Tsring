f27b4efdcbbe3609f9438a00eedf5f59a3e2e59d
Sonam Tsring <sonam.tsringyahoo.com@gmail.com>
2026-09-04 13:37:35 -0400

    I made it more colorful

---
+++ SS1 ST.T/sketch.js
@@ -0,0 +1,30 @@
+
+function setup() {
+  createCanvas(600, 600);
+createCanvas(600, 600, WEBGL);
+
+  background(150, 350, 350);
+  fill(100,100,100)
+  rect(-180, -25, 180, 60);
+  fill(250, 80, 100, 95)
+  circle(7.5, 20, 25)
+  circle(27.5, 35, 25)
+  circle(47.5, 50, 25)
+  circle(67.5, 65, 25)
+  circle(87.5, 80, 25)
+   circle(7.5, -5, 25)
+  circle(27.5, -20, 25)
+  circle(47.5, -35, 25)
+  circle(67.5, -50, 25)
+  circle(87.5, -65, 25)
+  circle(75.5, -87.5, 25)
+  circle(75.5, 102.5, 25)
+  circle(54.5, -100.5, 25)
+  circle(52.5, 112.5, 25)
+  fill(200, 50, 100)
+  triangle(-240, 5, -180, -50, -180, 55)
+
+  describe('A white circle with black outline in the middle of a gray canvas.')
+}
+function draw() {
+}
---
+++ function setup() {.js
@@ -0,0 +1,26 @@
+sketch.js
+
+
+function setup() {
+  createCanvas(600, 600);
+createCanvas(600, 600, WEBGL);
+
+  background(100);
+
+  circle(7.5, 20, 25);
+  circle(27.5, 35, 25)
+  circle(47.5, 50, 25)
+  circle(67.5, 65, 25)
+  circle(87.5, 80, 25)
+   circle(7.5, -5, 25)
+  circle(27.5, -20, 25)
+  circle(47.5, -35, 25)
+  circle(67.5, -50, 25)
+  circle(87.5, -65, 25)
+  
+
+  describe('A white circle with black outline in the middle of a gray canvas.');
+}
+
+function drawg(grey) {
+  background(220);
